984f6aacd88fca5f38ddfe3017485def3de349b6
Page Kyle <d3p747@constance03.pnl.gov>
2018-11-16 12:07:29 -0800

    correct logstart statement for case sentivity (L2041.resbio_input_irr.R)

--- input/gcam-data-system/aglu-processing-code/level2/L2041.resbio_input_irr.R
+++ input/gcam-data-system/aglu-processing-code/level2/L2041.resbio_input_irr.R
@@ -12,7 +12,7 @@ if( !exists( "AGLUPROC_DIR" ) ){
 # Universal header file - provides logging, file support, etc.
 source(paste(AGLUPROC_DIR,"/../_common/headers/GCAM_header.R",sep=""))
 source(paste(AGLUPROC_DIR,"/../_common/headers/AGLU_header.R",sep=""))
-logstart( "L2041.resbio_input_IRR.R" )
+logstart( "L2041.resbio_input_irr.R" )
 adddep(paste(AGLUPROC_DIR,"/../_common/headers/GCAM_header.R",sep=""))
 adddep(paste(AGLUPROC_DIR,"/../_common/headers/AGLU_header.R",sep=""))
 printlog( "Model input for residue biomass production from agriculture / forestry / milling" )
