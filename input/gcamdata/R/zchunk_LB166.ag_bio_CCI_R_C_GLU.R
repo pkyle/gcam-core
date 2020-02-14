e0aed80ce71f481c91a96c7f936d7fc316370309
Zhang <ying.zhang@pnnl.gov>
2020-02-14 16:18:43 -0500

    Finalize script aglu_LB166, comments added

--- input/gcamdata/R/zchunk_LB166.ag_bio_CCI_R_C_GLU.R
+++ input/gcamdata/R/zchunk_LB166.ag_bio_CCI_R_C_GLU.R
@@ -58,6 +58,7 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
     ISI_MIP_2085_pdssat_rcp8p5_ir <- get_data(all_data,"aglu/ISI-MIP/ISI_MIP_2085_pdssat_rcp8p5_ir")
     ISI_MIP_2085_pdssat_rcp8p5_rf <- get_data(all_data,"aglu/ISI-MIP/ISI_MIP_2085_pdssat_rcp8p5_rf")
 
+    # Add new column to differenciate files by year and irr level before binding them
     ISI_MIP_2030_pdssat_rcp2p6_ir$other = "2030_irr"
     ISI_MIP_2030_pdssat_rcp2p6_rf$other = "2030_rfd"
     ISI_MIP_2030_pdssat_rcp8p5_ir$other = "2030_irr"
@@ -67,7 +68,7 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
     ISI_MIP_2085_pdssat_rcp8p5_ir$other = "2085_irr"
     ISI_MIP_2085_pdssat_rcp8p5_rf$other = "2085_rfd"
 
-
+    # Move added column at the end to the begining
     ISI_MIP_2030_pdssat_rcp2p6_ir<-ISI_MIP_2030_pdssat_rcp2p6_ir %>% select(other,everything())
     ISI_MIP_2030_pdssat_rcp2p6_rf<-ISI_MIP_2030_pdssat_rcp2p6_rf %>% select(other,everything())
     ISI_MIP_2030_pdssat_rcp8p5_ir<-ISI_MIP_2030_pdssat_rcp8p5_ir %>% select(other,everything())
@@ -77,13 +78,16 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
     ISI_MIP_2085_pdssat_rcp8p5_ir<-ISI_MIP_2085_pdssat_rcp8p5_ir %>% select(other,everything())
     ISI_MIP_2085_pdssat_rcp8p5_rf<-ISI_MIP_2085_pdssat_rcp8p5_rf %>% select(other,everything())
 
+    # Bingding all AgMIP files into one table
     ISI_MIP_df <- bind_rows(ISI_MIP_2030_pdssat_rcp2p6_ir,ISI_MIP_2030_pdssat_rcp2p6_rf,ISI_MIP_2030_pdssat_rcp8p5_ir,
                             ISI_MIP_2030_pdssat_rcp8p5_rf,ISI_MIP_2085_pdssat_rcp2p6_ir,ISI_MIP_2085_pdssat_rcp2p6_rf,
                             ISI_MIP_2085_pdssat_rcp8p5_ir,ISI_MIP_2085_pdssat_rcp8p5_rf)
 
+    # Separate long strings in the file into new column names
     ISI_MIP_df<-separate(ISI_MIP_df,crop, c("crop_model", "climate_model","rcp","co2_fertilization","crop_name"),sep = "_")
     ISI_MIP_df<-separate(ISI_MIP_df,other, c("year", "irr_level"),sep = "_")
 
+    # Convert growth rate into yield ratio for year 2030 (assuming 20 years between 2010 & 2030) and year 2085 (assuming 75 years between ...)
     GrowthRate2030<-ISI_MIP_df %>% filter(year==2030)
     selNum<-unlist(lapply(GrowthRate2030,is.numeric))
     GrowthRate2030<-GrowthRate2030[,selNum]
@@ -94,34 +98,40 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
     GrowthRate2085<-GrowthRate2085[,selNum]
     YieldRatio2085<-(1+GrowthRate2085)^(2085-2010)
 
+    # Select numeric values (Growth rate) in the table and subsitute with new YieldRatio values
     YieldRatio <-ISI_MIP_df %>% select(-crop_model,-climate_model,-co2_fertilization)
     selNum<-unlist(lapply(YieldRatio,is.numeric))
 
     YieldRatio[which(YieldRatio$year=='2030'),selNum]<-YieldRatio2030
     YieldRatio[which(YieldRatio$year=='2085'),selNum]<-YieldRatio2085
 
+    # Gather YieldRatio table into long format and then spread years for interpolate values over years row by row
     long_YieldRatio <- YieldRatio %>%
       gather(iso,yield_ratio,-year,-irr_level,-rcp,-crop_name) %>%
       mutate(iso = tolower(iso))
     Wide_YieldRatio <- long_YieldRatio %>% spread(year,yield_ratio)
 
+    # Create a frame for interpolating YieldRatio over years using the for loop
     final_yieldRatio <- Wide_YieldRatio
-    final_yieldRatio$`2010` <- 1
+    final_yieldRatio$`2010` <- 1 # yield ratio at base year should be 1
     addCol <- setdiff(seq(2010,2100,5),c(2010,2030,2085))
     addCol <- as.character(addCol)
     final_yieldRatio[addCol]<-NA
     final_yieldRatio<-final_yieldRatio[,c(1:4,7:10,5,11:20,6,21:23)]
     yearseq <-seq(2010,2100,5)
 
+    # Interpolate YieldRatio for 2010, 2015,...,2100 based on values in 2010, 2030, and 2085
     for(i in 1:nrow(final_yieldRatio)){
       temp <- final_yieldRatio[i,5:23]
       tempIntp <- approx_fun(yearseq, as.numeric(temp), rule = 2)
       final_yieldRatio[i,5:23] <- tempIntp
     }
 
-# At this point we apply the yield ratios in each year to the irrigated and rainfed production volumes from the base year
-# This sequence first merges in the irrigated and rainfed production by country, crop, and basin, adding the necessary
+    # At this point we apply the yield ratios in each year to the irrigated and rainfed production volumes from the base year
+    # This sequence first merges in the irrigated and rainfed production by country, crop, and basin, adding the necessary
     # identifier column. PDSSAT impacts crop is joined in from the main crop mapping file, and then the join can be performed
+
+    # Join irr and rfd production in the base year
     L166.ag_Prod_t_ctry_crop_irr <- mutate(L151.ag_irrProd_t_ctry_crop, irr_level = "irr") %>%
       rename(Prod = irrProd) %>%
       bind_rows(mutate(L151.ag_rfdProd_t_ctry_crop, irr_level = "rfd") %>%
@@ -130,6 +140,7 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
                                by = "GTAP_crop") %>%
       drop_na(pdssat_crop)
 
+    # Calucate production (prod_x_yield_ratio) over years given calculated AgMIP yield ratio
     # Note - using left_join so that the data table can expand by the number of RCP emissions scenarios
     L166.ag_Prod_t_ctry_crop_irr_Y <- L166.ag_Prod_t_ctry_crop_irr %>%
       left_join(final_yieldRatio,
@@ -138,30 +149,20 @@ module_aglu_LB166.ag_bio_CCI_R_C_GLU <- function(command, ...) {
       gather(year,yield_ratio,as.character(seq(2010,2100,5))) %>%
       mutate(prod_x_yield_ratio = Prod * yield_ratio)
 
-    # making it into smaller steps so that R has enough memeory to process it (Error: cannot allocate vector of size 2.6 Gb)
+    # Map to GCAM regions and GCAM commodities
     L166.ag_YieldRatio_R_C_GLU_irr_Y<-L166.ag_Prod_t_ctry_crop_irr_Y %>%
       left_join_error_no_match(select(iso_GCAM_regID,iso,GCAM_region_ID),by="iso") %>%
       select(-iso,-yield_ratio) %>%
       left_join_error_no_match(select(FAO_ag_items_PRODSTAT,GTAP_crop,GCAM_commodity),by="GTAP_crop",
                                ignore_columns = "GCAM_commodity") %>%
       drop_na(GCAM_commodity) %>%
-      group_by(GCAM_region_ID, GCAM_commodity, GLU, rcp, year) %>%
+      group_by(GCAM_region_ID, GCAM_commodity, GLU, irr_level, rcp, year) %>%
       summarise(prod_x_yield_ratio = sum(prod_x_yield_ratio),
                 Prod = sum(Prod)) %>%
       ungroup() %>%
       mutate(YieldRatio = prod_x_yield_ratio / Prod)
 
-
-    ### STOPPED HERE - need to select the relevant columns and return this as a final table. Should have probably
-    ### named this table "L166.YieldRatio_R_C_Y_GLU_irr_CCIscen"
-
-    L166.YieldRatio_R_C_Y_GLU_irr_CCIscen<-L166.ag_Prod_t_ctry_crop_irr_Y %>% spread(year,value)
-
-    for(i in 1:nrow(L166.YieldRatio_R_C_Y_GLU_irr_CCIscen)){
-      temp <- L166.YieldRatio_R_C_Y_GLU_irr_CCIscen[i,5:23]
-      tempCalc <- temp/temp[1]
-      L166.YieldRatio_R_C_Y_GLU_irr_CCIscen[i,5:23] <- tempCalc
-    }
+    L166.YieldRatio_R_C_Y_GLU_irr_CCIscen<-select(L166.ag_YieldRatio_R_C_GLU_irr_Y,GCAM_region_ID,GCAM_commodity,year,GLU,irr_level,rcp,YieldRatio)
 
     # Produce outputs
     L166.YieldRatio_R_C_Y_GLU_irr_CCIscen %>%
