7fd577eabfab0f3110cac6a1b396b148b0486a8d
wolf184 <wolf184@WE40096>
2021-12-16 12:48:10 -0500

    code correction to buildings module

--- input/gcamdata/R/zchunk_LA144.building_det_en.R
+++ input/gcamdata/R/zchunk_LA144.building_det_en.R
@@ -358,7 +358,7 @@ module_energy_LA144.building_det_en <- function(command, ...) {
       #left_join_error_no_match(L144.share_serv_fuel, by = c("region_GCAM3", "sector", "fuel", "service")) %>%
       left_join(L144.share_serv_fuel, by = c("region_GCAM3", "sector", "fuel", "service")) %>%
       rename(share_serv_fuel_RG3 = share_serv_fuel) %>%
-      # replace_na(list(share_serv_fuel_RG3 = 0, share_TFEbysector = 0, fuel_share_of_TFEbysector = 0)) %>%
+      replace_na(list(share_serv_fuel_RG3 = 0, share_TFEbysector = 0, fuel_share_of_TFEbysector = 0)) %>%
       # Energy_tot = energy consumption by country, sector, and fuel (not disaggregated to service)
       # Joining table does not have every combination, so NAs will be introduced
       left_join(L144.in_EJ_ctry_bld_Fi_Yh, by = c("iso", "sector", "fuel")) %>%
