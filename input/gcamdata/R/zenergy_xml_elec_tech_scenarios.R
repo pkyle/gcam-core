cbf6321eecbba1a4b0363988ccef399f3a816b1f
Page Kyle <pkyle@pnnl.gov>
2025-10-01 00:27:40 -0600

    reset temporary names of electric sector technology level xml files

--- input/gcamdata/R/zenergy_xml_elec_tech_scenarios.R
+++ input/gcamdata/R/zenergy_xml_elec_tech_scenarios.R
@@ -8,8 +8,9 @@
 #' @param ... other optional parameters, depending on command
 #' @return Depends on \code{command}: either a vector of required inputs,
 #' a vector of output names, or (if \code{command} is "MAKE") all
-#' the generated outputs: \code{wind_adv1.xml}. The corresponding file in the
-#' original data system was \code{batch_wind_adv_xml.R} (energy XML).
+#' the generated outputs: \code{elec_bio_low.xml}, \code{geo_tech_adv.xml}, \code{geo_low.xml},
+#' \code{nuclear_adv.xml}, \code{nuclear_low.xml}, \code{solar_adv.xml}, \code{solar_low.xml},
+#' \code{wind_adv.xml}, \code{wind_low.xml}.
 module_energy_elec_tech_scenarios_xml <- function(command, ...) {
 
   MODULE_INPUTS <-
@@ -26,15 +27,15 @@ module_energy_elec_tech_scenarios_xml <- function(command, ...) {
       "L223.StubTechCapFactor_elec")
 
   MODULE_OUTPUTS <-
-    c(XML = "elec_bio_low1.xml",
-      XML = "geo_tech_adv1.xml",
-      XML = "geo_low1.xml",
-      XML = "nuclear_adv1.xml",
-      XML = "nuclear_low1.xml",
-      XML = "solar_adv1.xml",
-      XML = "solar_low1.xml",
-      XML = "wind_adv1.xml",
-      XML = "wind_low1.xml")
+    c(XML = "elec_bio_low.xml",
+      XML = "geo_tech_adv.xml",
+      XML = "geo_low.xml",
+      XML = "nuclear_adv.xml",
+      XML = "nuclear_low.xml",
+      XML = "solar_adv.xml",
+      XML = "solar_low.xml",
+      XML = "wind_adv.xml",
+      XML = "wind_low.xml")
 
   if(command == driver.DECLARE_INPUTS) {
     return(MODULE_INPUTS)
@@ -113,82 +114,82 @@ module_energy_elec_tech_scenarios_xml <- function(command, ...) {
 
     # Produce outputs
     # Adv scenario XML files
-    create_xml("geo_tech_adv1.xml") %>%
+    create_xml("geo_tech_adv.xml") %>%
       add_xml_data(L223.GlobalTechCapital_geo_adv, "GlobalTechCapital") %>%
       add_precursors("L1233.globaltech_capital_ATB_adv",
                      "L1233.globaltech_OMfixed_ATB_adv",
                      "L1233.globaltech_OMvar_ATB_adv") ->
-      geo_tech_adv1.xml
+      geo_tech_adv.xml
 
-    create_xml("nuclear_adv1.xml") %>%
+    create_xml("nuclear_adv.xml") %>%
       add_xml_data(L223.GlobalTechCapital_nuc_adv, "GlobalTechCapital") %>%
       add_xml_data(L125.nuclear_hydrogen_costs_adv, "GlobalTechCost") %>%
-      same_precursors_as(geo_tech_adv1.xml) %>%
+      same_precursors_as(geo_tech_adv.xml) %>%
       add_precursors("L125.nuclear_hydrogen_costs_adv") ->
-      nuclear_adv1.xml
+      nuclear_adv.xml
 
-    create_xml("solar_adv1.xml") %>%
+    create_xml("solar_adv.xml") %>%
       add_xml_data(L223.GlobalTechCapital_sol_adv, "GlobalTechCapital") %>%
       add_xml_data(L223.GlobalIntTechCapital_sol_adv, "GlobalIntTechCapital") %>%
       add_xml_data(L223.StubTechCost_roofpv_adv, "StubTechCost") %>%
-      same_precursors_as(geo_tech_adv1.xml) %>%
+      same_precursors_as(geo_tech_adv.xml) %>%
       add_precursors("energy/A23.globalinttech",
                      "L223.StubTechCapFactor_elec") ->
-      solar_adv1.xml
+      solar_adv.xml
 
-    create_xml("wind_adv1.xml") %>%
+    create_xml("wind_adv.xml") %>%
       add_xml_data(L223.GlobalTechCapital_wind_adv, "GlobalTechCapital") %>%
       add_xml_data(L223.GlobalIntTechCapital_wind_adv, "GlobalIntTechCapital") %>%
-      same_precursors_as(geo_tech_adv1.xml) %>%
+      same_precursors_as(geo_tech_adv.xml) %>%
       add_precursors("energy/A23.globalinttech") ->
-      wind_adv1.xml
+      wind_adv.xml
 
     # Low scenario XML files
-    create_xml("elec_bio_low1.xml") %>%
+    create_xml("elec_bio_low.xml") %>%
       add_xml_data(L223.GlobalTechCapital_bio_low, "GlobalTechCapital") %>%
       add_precursors("L1233.globaltech_capital_ATB_low",
                      "L1233.globaltech_OMfixed_ATB_low",
                      "L1233.globaltech_OMvar_ATB_low") ->
-      elec_bio_low1.xml
+      elec_bio_low.xml
 
-    create_xml("geo_low1.xml") %>%
+    create_xml("geo_low.xml") %>%
       add_xml_data(L223.GlobalTechCapital_geo_low, "GlobalTechCapital") %>%
-      same_precursors_as(elec_bio_low1.xml) ->
-      geo_low1.xml
+      same_precursors_as(elec_bio_low.xml) ->
+      geo_low.xml
 
-    create_xml("nuclear_low1.xml") %>%
+    create_xml("nuclear_low.xml") %>%
       add_xml_data(L223.GlobalTechCapital_nuc_low, "GlobalTechCapital") %>%
       add_xml_data(L125.nuclear_hydrogen_costs_low, "GlobalTechCost") %>%
-      same_precursors_as(elec_bio_low1.xml) %>%
+      same_precursors_as(elec_bio_low.xml) %>%
       add_precursors("L125.nuclear_hydrogen_costs_low") ->
-      nuclear_low1.xml
+      nuclear_low.xml
 
-    create_xml("solar_low1.xml") %>%
+    create_xml("solar_low.xml") %>%
       add_xml_data(L223.GlobalTechCapital_sol_low, "GlobalTechCapital") %>%
       add_xml_data(L223.GlobalIntTechCapital_sol_low, "GlobalIntTechCapital") %>%
       add_xml_data(L223.StubTechCost_roofpv_low, "StubTechCost") %>%
-      same_precursors_as(elec_bio_low1.xml) %>%
+      same_precursors_as(elec_bio_low.xml) %>%
       add_precursors("energy/A23.globalinttech",
                      "L223.StubTechCapFactor_elec") ->
-      solar_low1.xml
+      solar_low.xml
 
-    create_xml("wind_low1.xml") %>%
+    create_xml("wind_low.xml") %>%
       add_xml_data(L223.GlobalTechCapital_wind_low, "GlobalTechCapital") %>%
       add_xml_data(L223.GlobalIntTechCapital_wind_low, "GlobalIntTechCapital") %>%
-      same_precursors_as(elec_bio_low1.xml) %>%
+      same_precursors_as(elec_bio_low.xml) %>%
       add_precursors("energy/A23.globalinttech") ->
-      wind_low1.xml
+      wind_low.xml
 
 
-    return_data(geo_tech_adv1.xml,
-                nuclear_adv1.xml,
-                solar_adv1.xml,
-                wind_adv1.xml,
-                elec_bio_low1.xml,
-                geo_low1.xml,
-                nuclear_low1.xml,
-                solar_low1.xml,
-                wind_low1.xml)
+    return_data(geo_tech_adv.xml,
+                nuclear_adv.xml,
+                solar_adv.xml,
+                wind_adv.xml,
+                elec_bio_low.xml,
+                geo_low.xml,
+                nuclear_low.xml,
+                solar_low.xml,
+                wind_low.xml)
   } else {
     stop("Unknown command")
   }
